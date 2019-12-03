32f05265bcea503591cdb43df89c3b052cfa2302
Chad Groft <chad.groft@capitalone.com>
2019-12-03 11:35:18 -0500

    Clean up day 3

--- day03.rb
+++ day03.rb
@@ -1,56 +1,41 @@
+def wire_distances(wire_codes)
+  x, y = 0, 0
+  distance_so_far = 0
+  all_distances = {}
+
+  wire_codes.each do |code|
+    direction, distance = code[0], code[1..-1].to_i
+    distance.times do
+      case direction
+      when "U"
+        y += 1
+      when "D"
+        y -= 1
+      when "L"
+        x -= 1
+      when "R"
+        x += 1
+      end
+
+      distance_so_far += 1
+      all_distances[[x, y]] ||= distance_so_far
+    end
+  end
+
+  all_distances
+end
+
 wire1 = gets.strip.split(",")
 wire2 = gets.strip.split(",")
 
-wire_codes = Hash.new { |h,k| h[k] = 0 }
-wire1_distance = {}
-wire2_distance = {}
+wire1_distance = wire_distances(wire1)
+wire2_distance = wire_distances(wire2)
 
-x, y = 0, 0
-distance_so_far = 0
-wire1.each do |code|
-  direction, distance = code[0], code[1..-1].to_i
-  distance.times do
-    case direction
-    when "U"
-      y += 1
-    when "D"
-      y -= 1
-    when "L"
-      x -= 1
-    when "R"
-      x += 1
-    end
+intersections = wire1_distance.keys.select { |k| wire2_distance.has_key?(k) }
 
-    wire_codes[[x, y]] |= 1
-    distance_so_far += 1
-    wire1_distance[[x, y]] ||= distance_so_far
-  end
-end
+shortest_taxicab_distance = intersections.collect { |k| k.map(&:abs).reduce(0, :+) }.min
+shortest_wire_distance = intersections.collect { |k| wire1_distance[k] + wire2_distance[k] }.min
 
-x, y = 0, 0
-distance_so_far = 0
-wire2.each do |code|
-  direction, distance = code[0], code[1..-1].to_i
-  distance.times do
-    case direction
-    when "U"
-      y += 1
-    when "D"
-      y -= 1
-    when "L"
-      x -= 1
-    when "R"
-      x += 1
-    end
+puts "Shortest taxicab distance: #{shortest_taxicab_distance}"
+puts "Shortest wire distance: #{shortest_wire_distance}"
 
-    wire_codes[[x, y]] |= 2
-    distance_so_far += 1
-    wire2_distance[[x, y]] ||= distance_so_far
-  end
-end
-
-shortest_distance = wire_codes.keys
-  .select { |k| wire_codes[k] == 3 }
-  .collect { |k| wire1_distance[k] + wire2_distance[k] } # .collect { |k| k.map(&:abs).reduce(0, :+) }
-  .min
-print shortest_distance, "\n"
