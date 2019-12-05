a007a9ab09d31dcd09f5e82d1d97ce2a57fd3fd3
Chad Groft <chad.groft@capitalone.com>
2019-12-05 13:40:42 -0500

    Day 5: complete part 1

--- day05.rb
+++ day05.rb
@@ -70,7 +70,10 @@ class Intcode
 
       when READ_OPCODE
         input = take_input
-        puts "Error: no input available" and exit 1 unless input
+        unless input
+          puts "Error: no input available"
+          exit 1
+        end
         set_value(1, input)
         @pc += 2
 
@@ -87,25 +90,8 @@ class Intcode
   end
 end
 
-def check_input(initial_memory, noun, verb)
-  computer = Intcode.new(initial_memory)
-  computer.set_register(1, noun)
-  computer.set_register(2, verb)
-  computer.run_program
-  result = computer.get_register(0)
-  puts "noun = #{noun}, verb = #{verb}" and exit 0 if result == 19690720
-end
-
 initial_memory = gets.split(",").map(&:to_i)
+computer = Intcode.new(initial_memory)
+computer.add_input(1)
+computer.run_program { |n| puts "Output: #{n}" }
 
-(0...initial_memory.size).each do |n|
-  (0..n).each do |noun|
-    verb = n
-    check_input(initial_memory, noun, verb)
-  end
-
-  (0...n).each do |verb|
-    noun = n
-    check_input(initial_memory, noun, verb)
-  end
-end
