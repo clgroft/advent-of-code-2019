624f1a960e6499de6dfe3fc8da851783d15194c2
Chad Groft <chad.groft@capitalone.com>
2019-12-09 16:10:53 -0500

    Translate day 1 to Ruby

--- day01.hs
+++
@@ -1,7 +0,0 @@
-main = getContents >>= print . sum . map totalFuelCost . map read . lines
-
-basicFuelCost :: Integer -> Integer
-basicFuelCost n = (n `div` 3) - 2
-
-totalFuelCost :: Integer -> Integer
-totalFuelCost = sum . takeWhile (>0) . iterate basicFuelCost . basicFuelCost
---
+++ day01.rb
@@ -0,0 +1,22 @@
+def basic_fuel_cost(weight)
+  weight / 3 - 2
+end
+
+def total_fuel_cost(weight)
+  total_cost = 0
+  partial_fuel_cost = basic_fuel_cost(weight)
+  while partial_fuel_cost > 0
+    total_cost += partial_fuel_cost
+    partial_fuel_cost = basic_fuel_cost(partial_fuel_cost)
+  end
+  total_cost
+end
+
+weights = ARGF.each_line.map { |line| line.strip.to_i }
+
+part1_cost = weights.map { |w| basic_fuel_cost(w) }.inject(0, :+)
+puts "Basic fuel cost: #{part1_cost}"
+
+part2_cost = weights.map { |w| total_fuel_cost(w) }.inject(0, :+)
+puts "Total fuel cost: #{part2_cost}"
+
