509653fe121b3c36ac071eba0c7686142821e30b
Chad Groft <chad.groft@capitalone.com>
2019-12-11 10:41:34 -0500

    Day 11: refactor

--- day11.rb
+++ day11.rb
@@ -2,11 +2,11 @@
 
 require_relative 'lib/intcode'
 
-class Colors
+
+class Color
   BLACK = 0
   WHITE = 1
 end
-
 
 class Direction
   UP = 0
@@ -20,11 +20,11 @@ class RotateDirection
   CLOCKWISE = 1
 end
 
-def turn_left(dir)
+def turn_counterclockwise(dir)
   (dir + 1) % 4
 end
 
-def turn_right(dir)
+def turn_clockwise(dir)
   (dir + 3) % 4
 end
 
@@ -37,7 +37,7 @@ class HullPaintingRobot
   end
 
   def start_on_white
-    @cells[[0,0]] = Colors::WHITE
+    @cells[[0,0]] = Color::WHITE
   end
 
   def paint_hull
@@ -45,7 +45,7 @@ class HullPaintingRobot
     direction = Direction::UP
 
     loop do
-      @intcode.add_input(@cells[[x,y]] || Colors::BLACK)
+      @intcode.add_input(get_color(x, y))
       outputs = []
       @intcode.run_program { |out| outputs << out }
       case outputs.length
@@ -53,32 +53,9 @@ class HullPaintingRobot
         return
       when 2
         color, rotate = outputs
-
-        @cells[[x, y]] = color
-
-        case rotate
-        when RotateDirection::COUNTERCLOCKWISE
-          direction = turn_left(direction)
-        when RotateDirection::CLOCKWISE
-          direction = turn_right(direction)
-        else
-          puts "Illegal rotate direction #{rotate}"
-          exit 1
-        end
-
-        case direction
-        when Direction::UP
-          y -= 1
-        when Direction::LEFT
-          x -= 1
-        when Direction::DOWN
-          y += 1
-        when Direction::RIGHT
-          x += 1
-        else
-          puts "Illegal direction #{direction}"
-          exit 1
-        end
+        set_color(x, y, color)
+        direction = new_direction(direction, rotate)
+        x, y = new_xy(x, y, direction)
       else
         puts "Output #{outputs} should have length 0 or 2"
         exit 1
@@ -96,9 +73,45 @@ class HullPaintingRobot
 
     (y_min..y_max).map do |y|
       (x_min..x_max).map do |x|
-        @cells[[x,y]] == Colors::WHITE ? "*" : " "
+        @cells[[x,y]] == Color::WHITE ? "*" : " "
       end.join + "\n"
     end.join
+  end
+
+  def get_color(x, y)
+    @cells[[x,y]] || Color::BLACK
+  end
+
+  def set_color(x, y, color)
+    @cells[[x,y]] = color
+  end
+
+  def new_direction(direction, rotate)
+    case rotate
+    when RotateDirection::COUNTERCLOCKWISE
+      turn_counterclockwise(direction)
+    when RotateDirection::CLOCKWISE
+      turn_clockwise(direction)
+    else
+      puts "Illegal rotate direction #{rotate}"
+      exit 1
+    end
+  end
+
+  def new_xy(x, y, direction)
+    case direction
+    when Direction::UP
+      [x, y-1]
+    when Direction::LEFT
+      [x-1, y]
+    when Direction::DOWN
+      [x, y+1]
+    when Direction::RIGHT
+      [x+1, y]
+    else
+      puts "Illegal direction #{direction}"
+      exit 1
+    end
   end
 end
 
@@ -106,6 +119,7 @@ program = ARGF.read.strip.split(',').map(&:to_i)
 robot = HullPaintingRobot.new(program)
 robot.paint_hull
 puts "Number of cells painted: #{robot.number_cells_painted}"
+puts
 
 robot = HullPaintingRobot.new(program)
 robot.start_on_white
