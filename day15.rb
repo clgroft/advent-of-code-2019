69c5d5e14b070edd301b767a49419a89e8e37f67
Chad Groft <clgroft@gmail.com>
2019-12-15 10:13:45 -0500

    Day 15: solve part 2

--- day15.rb
+++ day15.rb
@@ -25,36 +25,124 @@ def new_coordinates(start_coordinates, direction)
   end
 end
 
-initial_memory = gets.split(",").map(&:to_i)
-starting_computer = Intcode.new(initial_memory)
+def find_oxygen_system(starting_computer)
+  num_steps = {[0,0] => 0} # zero steps from the origin to the origin
+  queue = [[[0,0], starting_computer]]
+  system_position = nil
+  system_steps = nil
 
-num_steps = {[0,0] => 0} # zero steps from the origin to the origin
-queue = [[[0,0], starting_computer]]
-
-until queue.empty?
-  start_coordinates, intcode_state = queue.shift
-  (NORTH..EAST).each do |dir|
-    finish_coordinates = new_coordinates(start_coordinates, dir)
-    next if num_steps.has_key?(finish_coordinates)
-    new_intcode_state = intcode_state.dup
-    new_intcode_state.add_input(dir)
-    new_intcode_state.run_program do |status|
-      case status
-      when WALL
-        num_steps[finish_coordinates] = :wall
-      when EMPTY_SPACE
-        num_steps[finish_coordinates] = num_steps[start_coordinates] + 1
-        queue << [finish_coordinates, new_intcode_state]
-      when FOUND_SYSTEM
-        puts "Found oxygen system in #{num_steps[start_coordinates] + 1} steps"
-        exit 0
+  until queue.empty?
+    start_coordinates, intcode_state = queue.shift
+    (NORTH..EAST).each do |dir|
+      finish_coordinates = new_coordinates(start_coordinates, dir)
+      next if num_steps.has_key?(finish_coordinates)
+      new_intcode_state = intcode_state.dup
+      new_intcode_state.add_input(dir)
+      new_intcode_state.run_program do |status|
+        case status
+        when WALL
+          num_steps[finish_coordinates] = :wall
+        when EMPTY_SPACE
+          num_steps[finish_coordinates] = num_steps[start_coordinates] + 1
+          queue << [finish_coordinates, new_intcode_state]
+        when FOUND_SYSTEM
+          system_position = finish_coordinates
+          system_steps = num_steps[start_coordinates] + 1
+          num_steps[finish_coordinates] = system_steps
+          queue << [finish_coordinates, new_intcode_state]
+        end
       end
     end
   end
+
+  [system_position, system_steps, num_steps]
 end
 
-puts "Ran out of places to go but couldn't find oxygen system!"
-exit 1
+def time_to_fill_with_oxygen(system_coordinates, starting_computer)
+  num_minutes = {system_coordinates => 0}
+  queue = [[system_coordinates, starting_computer]]
 
-# computer.run_program { |n| puts "Output: #{n}" }
+  until queue.empty?
+    start_coordinates, intcode_state = queue.shift
+    (NORTH..EAST).each do |dir|
+      finish_coordinates = new_coordinates(start_coordinates, dir)
+      next if num_minutes.has_key?(finish_coordinates)
+      new_intcode_state = intcode_state.dup
+      new_intcode_state.add_input(dir)
+      new_intcode_state.run_program do |status|
+        case status
+        when WALL
+          num_minutes[finish_coordinates] = :wall
+        when EMPTY_SPACE
+          num_minutes[finish_coordinates] = num_minutes[start_coordinates] + 1
+          queue << [finish_coordinates, new_intcode_state]
+        when FOUND_SYSTEM
+          puts "Found another oxygen system at #{finish_coordinates}!"
+          exit 1
+        end
+      end
+    end
+  end
+
+  num_minutes.values.reject { |n| n == :wall }.max
+end
+
+def size_of_region_map(region_map, system_coordinates)
+  num_minutes = {system_coordinates => 0}
+  queue = [system_coordinates]
+  max_minutes = 0
+
+  until queue.empty?
+    start_coordinates = queue.shift
+    next_minutes = num_minutes[start_coordinates] + 1
+    (NORTH..EAST).each do |dir|
+      finish_coordinates = new_coordinates(start_coordinates, dir)
+      next if num_minutes.has_key?(finish_coordinates)
+      next if region_map[finish_coordinates] == :wall
+      # puts "#{finish_coordinates}: #{next_minutes}"
+      num_minutes[finish_coordinates] = next_minutes
+      max_minutes = next_minutes
+      queue << finish_coordinates
+    end
+  end
+  max_minutes
+end
+
+initial_memory = gets.split(",").map(&:to_i)
+starting_computer = Intcode.new(initial_memory)
+
+oxygen_system_coordinates, num_steps, region_map = find_oxygen_system(starting_computer)
+puts "Found oxygen system in #{num_steps} steps at #{oxygen_system_coordinates}"
+
+x_min, x_max = region_map.keys.map { |x, _y| x }.minmax
+y_min, y_max = region_map.keys.map { |_x, y| y }.minmax
+picture = (y_min..y_max).map do |y|
+  (x_min..x_max).map do |x|
+    case region_map[[x,y]]
+    when nil
+      '*'
+    when :wall
+      '*'
+    else
+      case [x,y]
+      when oxygen_system_coordinates
+        '@'
+      when [0,0]
+        '#'
+      else
+        ' '
+      end
+    end
+  end.join + "\n"
+end.join
+
+puts
+puts picture
+puts
+
+oxygen_fill_time = size_of_region_map(region_map, oxygen_system_coordinates)
+puts "Oxygen will fill region in #{oxygen_fill_time} minutes"
+
+# time_until_full = time_to_fill_with_oxygen(oxygen_system_coordinates, intcode_state)
+# puts "Full oxygenation in #{time_until_full} minutes"
 
