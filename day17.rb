758a4a5fea21f6d2860cd4ee60fd83ebe6db2a2a
Chad Groft <chad.groft@capitalone.com>
2019-12-17 12:28:57 -0500

    Day 17: solve part 2

--- day17.rb
+++ day17.rb
@@ -5,10 +5,11 @@ require_relative 'lib/intcode'
 initial_memory = gets.split(",").map(&:to_i)
 computer = Intcode.new(initial_memory)
 
+# Part 1: find and add the intersection alignment parameters
 camera_feed = []
 computer.run_program { |n| camera_feed << n.chr }
 picture = camera_feed.join
-puts picture
+# puts picture
 
 picture_array = picture.split("\n").map { |l| l.split('') }
 num_rows = picture_array.length
@@ -24,4 +25,41 @@ alignment_params_sum =
     end.inject(0, :+)
   end.inject(0, :+)
 puts "Sum of alignment parameters: #{alignment_params_sum}"
+puts
+
+# Part 2: find a path through the scaffolding, compress it
+# into three subroutines (I did this by hand, *no* shame),
+# and have the "vacuum robot" execute the result
+
+procedure_A = "R,6,L,6,L,10"
+procedure_B = "L,8,L,6,L,10,L,6"
+procedure_C = "R,6,L,8,L,10,R,6"
+main_procedure = "A,B,A,B,C,A,B,C,A,C"
+options = "n"
+inputs = [
+  main_procedure,
+  procedure_A,
+  procedure_B,
+  procedure_C,
+  options
+].map { |str| str + "\n" }
+
+initial_memory[0] = 2
+computer = Intcode.new(initial_memory)
+
+puts "Starting image:"
+puts
+inputs.each do |input|
+  out_chars = []
+  computer.run_program { |out| out_chars << out.chr }
+  puts out_chars.join + input
+  input.chars.each { |c| computer.add_input(c.ord) }
+end
+
+puts
+puts "Image when done:"
+outputs = []
+computer.run_program { |out| outputs << out }
+puts outputs[0...outputs.size-1].map(&:chr).join
+puts "Units of dust collected: #{outputs[-1]}"
 
