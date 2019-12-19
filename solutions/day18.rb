693ed20bbfa3c4c62fcf1ac4c5322d375cf206a7
Chad Groft <chad.groft@capitalone.com>
2019-12-19 12:28:46 -0500

    Day 18: solve part 1

--- solutions/day18.rb
+++ solutions/day18.rb
@@ -1,33 +1,98 @@
 #!/usr/bin/env ruby
 
 require 'set'
-
+require 'pqueue'  # gem install pqueue
 
 class KeyPathSearch
   def initialize(tunnel_map)
     @tunnel_map = tunnel_map
     find_entrance
-    @num_keys = tunnel_map.map { |row| row.scan(/[a-z]/).size }.inject(:+)
+    find_all_keys
+    find_all_paths_between_keys
+
+    # @tunnel_map.each { |row| puts row }
+    # puts "Entrance is at #{@entrance}"
+    # pp @key_locations
+    # pp @paths_to_keys
   end
 
-  def fewest_steps_to_all_keys(location=@entrance, keys_held=Set[], keys_to_find=@num_keys)
-    return 0 if keys_to_find == 0
-    accessible_keys = find_accessible_keys(location, keys_held)
-    keys_left_to_find = keys_to_find - 1
-    accessible_keys.map do |key|
-      key[:distance] + fewest_steps_to_all_keys(
-        key[:location],
-        keys_held | [key[:name]],
-        keys_left_to_find)
-    end.min
+  def fewest_steps_to_all_keys
+    partial_solutions = PQueue.new([{
+      distance_so_far: 0,
+      location: @entrance,
+      keys_held: Set[],
+      keys_found_in_order: [],
+    }]) { |a, b| b[:distance_so_far] <=> a[:distance_so_far] }
+    shortest_distance_found = nil
+    shortest_distance_to_states = {}
+
+    until partial_solutions.empty?
+      current_attempt = partial_solutions.pop
+      # puts current_attempt
+      distance_so_far = current_attempt[:distance_so_far]
+
+      if shortest_distance_found && shortest_distance_found <= distance_so_far
+        # puts "Already have distance #{shortest_distance_found}"
+        return shortest_distance_found
+      end
+
+      keys_held = current_attempt[:keys_held]
+      if keys_held.size == @key_locations.size
+        # puts "Sequence #{current_attempt[:keys_found_in_order]} works in #{distance_so_far} steps"
+        shortest_distance_found = distance_so_far
+        next
+      end
+
+      location = current_attempt[:location]
+      state = {keys_held: keys_held, location: location}
+      shortest_to_here = shortest_distance_to_states[state]
+      next if shortest_to_here && shortest_to_here <= distance_so_far
+      shortest_distance_to_states[state] = distance_so_far
+
+      @paths_to_keys[location]
+        .reject { |path| keys_held.include?(path[:key]) }
+        .select { |path| path[:keys_needed] <= keys_held }
+        .each do |path|
+          partial_solutions << {
+            distance_so_far: distance_so_far + path[:distance],
+            location: path[:location],
+            keys_held: keys_held | Set[path[:key]],
+            keys_found_in_order: current_attempt[:keys_found_in_order] + [path[:key]]
+          }
+        end
+    end
+
+    shortest_distance_found
   end
+
+  # def fewest_steps_to_all_keys(location=@entrance, keys_held=Set[], pruning_length=nil)
+  #   puts "#{' ' * keys_held.size}#{location}: #{pruning_length}"
+  #
+  #   return 0 if pruning_length && pruning_length <= 0
+  #   return 0 if keys_held.size == @key_locations.size
+  #
+  #   paths_to_consider = @paths_to_keys[location]
+  #     .reject { |path| keys_held.include?(path[:key]) }
+  #     .select { |path| path[:keys_needed] <= keys_held }
+  #   best_length = pruning_length
+  #   paths_to_consider.each do |path|
+  #     path_distance = path[:distance]
+  #     best_length_from_path = path_distance + fewest_steps_to_all_keys(
+  #       path[:location],
+  #       keys_held | Set[path[:key]],
+  #       best_length ? best_length - path_distance : nil
+  #     )
+  #     best_length = best_length_from_path if best_length.nil? || best_length_from_path < best_length
+  #   end
+  #   best_length
+  # end
 
   private
 
   DIRECTIONS = {
     north: ->(i, j) { [i-1, j] },
-    east:  ->(i, j) { [i, j+1] },
     south: ->(i, j) { [i+1, j] },
+    east:  ->(i, j) { [i, j+1] },
     west:  ->(i, j) { [i, j-1] },
   }
 
@@ -41,37 +106,78 @@ class KeyPathSearch
       .select { |_i, j| j }
       .first
     i, j = @entrance
-    @tunnel_map[i][j] = OPEN_SPACE
+    set_contents(i, j, OPEN_SPACE)
   end
 
-  def find_accessible_keys(location, keys_held)
-    accessible_keys = []
-    known_distances = {location => 0}
-    location_queue = [location]
-    until location_queue.empty?
-      loc = location_queue.shift
-      DIRECTIONS.values.each do |move|
-        new_loc = move.call(*loc)
-        next if known_distances.has_key?(new_loc)
-        new_distance = known_distances[loc] + 1
-        known_distances[new_loc] = new_distance
-        i, j = *new_loc
-        contents = @tunnel_map[i][j]
-        case contents
-        when OPEN_SPACE
-          location_queue << new_loc
-        when DOOR
-          location_queue << new_loc if keys_held.include?(contents.downcase)
-        when KEY
-          if keys_held.include?(contents)
-            location_queue << new_loc
-          else
-            accessible_keys << {name: contents, location: new_loc, distance: new_distance}
-          end
+  def find_all_keys
+    @key_locations = {}
+    @tunnel_map.each_with_index do |row, i|
+      (0...row.length).each do |j|
+        char = row[j]
+        if KEY.include?(char)
+          @key_locations[char] = [i,j]
         end
       end
     end
-    accessible_keys
+  end
+
+  def find_all_paths_between_keys
+    @paths_to_keys = {}
+    [@entrance].concat(@key_locations.values).each do |loc|
+      @paths_to_keys[loc] = find_all_paths_from_location(loc)
+    end
+  end
+
+  def find_all_paths_from_location(location)
+    paths_to_keys = []
+    starting_contents = contents(*location)
+    known_paths = {location => {distance: 0, contents: starting_contents, keys_needed: Set[]}}
+    location_queue = [location]
+    until location_queue.empty?
+      loc = location_queue.shift
+      curr_path = known_paths[loc]
+
+      DIRECTIONS.values.each do |move|
+        new_loc = move.call(*loc)
+        new_contents = contents(*new_loc)
+        next if new_contents == '#' || known_paths.has_key?(new_loc)
+
+        location_queue << new_loc
+
+        curr_contents = curr_path[:contents]
+        known_paths[new_loc] = {
+          distance: curr_path[:distance] + 1,
+          contents: new_contents,
+          keys_needed: new_keys_needed(curr_path[:keys_needed], curr_contents, starting_contents),
+        }
+      end
+    end
+    known_paths
+      .select { |_k, v| KEY.include?(v[:contents]) }
+      .map do |k, v|
+        {
+          key: v[:contents],
+          keys_needed: v[:keys_needed],
+          distance: v[:distance],
+          location: k,
+        }
+      end
+  end
+
+  def contents(i, j)
+    @tunnel_map[i][j]
+  end
+
+  def set_contents(i, j, contents)
+    @tunnel_map[i][j] = contents
+  end
+
+  def new_keys_needed(curr_keys_needed, curr_contents, starting_contents)
+    if starting_contents != curr_contents && (DOOR.include?(curr_contents) || KEY.include?(curr_contents))
+      curr_keys_needed | Set[curr_contents.downcase]
+    else
+      curr_keys_needed
+    end
   end
 end
 
